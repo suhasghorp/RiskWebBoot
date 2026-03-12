2bc0d73b5a8a0b6b93616ab95366e0262728e297
suhasghorp <suhasghorp@gmail.com>
2026-03-12 18:11:42 -0400

    initial commit

---
+++ src/main/java/com/ab/riskweb/riskweb/RiskWebApplication.java
@@ -0,0 +1,13 @@
+package com.ab.riskweb.riskweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiskWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiskWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ab/riskweb/riskweb/RiskWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.riskweb.riskweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiskWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
